eba763aa33bcb7d370a8cd27be0f2a42d5779a8b
shikhachauhan99 <shikhachauhanrssis@gmail.com>
2026-08-25 11:02:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ function renderHTML({ title, meta, bodyContent, schema }) {
 <html lang="en">
 <head>
 <meta charset="UTF-8"/>
-//meta
+<meta name="google-site-verification" content="oJdcKbrJxTCibHfhezKJKDzrBBtnmHSuTaqXkw6gq5Y" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>${title}</title>
 <meta name="description" content="${meta}"/>
